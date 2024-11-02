ab0464bf96afbd615756d7c5c2b20bad0b65dc4c
chkan.dmytro <chkan.dv@gmail.com>
2024-11-02 20:14:19 +0200

    implemented SplashScreen

--- app/src/main/java/chkan/ua/shoppinglist/MainActivity.kt
+++ app/src/main/java/chkan/ua/shoppinglist/MainActivity.kt
@@ -9,12 +9,14 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import chkan.ua.shoppinglist.navigation.NavigationContainer
 import chkan.ua.shoppinglist.ui.theme.ShoppingListTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         enableEdgeToEdge()
         setContent {
             ShoppingListTheme {
