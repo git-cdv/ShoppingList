1fdd093ac7e9f62cb6d6a3fc6342a91efab175dc
chkan.dmytro <chkan.dv@gmail.com>
2024-11-04 17:49:42 +0200

    implemented Typography

--- app/src/main/java/chkan/ua/shoppinglist/ui/theme/Type.kt
+++ app/src/main/java/chkan/ua/shoppinglist/ui/theme/Type.kt
@@ -2,33 +2,39 @@ package chkan.ua.shoppinglist.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import chkan.ua.shoppinglist.R
+
+val fontFamily = FontFamily(
+    Font(R.font.montserrat_regular, weight = FontWeight.Normal),
+    Font(R.font.montserrat_semi_bold, weight = FontWeight.SemiBold),
+    Font(R.font.montserrat_bold, weight = FontWeight.Bold)
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = fontFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
+    ),
     titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
+        fontFamily = fontFamily,
+        fontWeight = FontWeight.SemiBold,
         fontSize = 22.sp,
         lineHeight = 28.sp,
         letterSpacing = 0.sp
     ),
     labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = fontFamily,
         fontWeight = FontWeight.Medium,
         fontSize = 11.sp,
         lineHeight = 16.sp,
         letterSpacing = 0.5.sp
     )
-    */
 )
