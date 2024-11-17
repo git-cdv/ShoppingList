f2f23f9242cd0d8a85729fbafbb392621e92cdf4
chkan.dmytro <chkan.dv@gmail.com>
2024-11-17 22:31:24 +0200

    start top bar

--- app/src/main/java/chkan/ua/shoppinglist/ui/screens/items/ItemsScreen.kt
+++ app/src/main/java/chkan/ua/shoppinglist/ui/screens/items/ItemsScreen.kt
@@ -2,17 +2,32 @@ package chkan.ua.shoppinglist.ui.screens.items
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.WindowInsetsSides
+import androidx.compose.foundation.layout.consumeWindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.only
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawing
+import androidx.compose.foundation.layout.statusBars
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -32,6 +47,7 @@ import chkan.ua.domain.models.Item
 import chkan.ua.shoppinglist.R
 import chkan.ua.shoppinglist.navigation.ItemsRoute
 import chkan.ua.shoppinglist.ui.kit.AddItemBottomSheet
+import chkan.ua.shoppinglist.ui.kit.TopBar
 import chkan.ua.shoppinglist.ui.kit.items.ItemItem
 import chkan.ua.shoppinglist.ui.kit.items.ReadyItem
 import chkan.ua.shoppinglist.ui.theme.ShoppingListTheme
@@ -42,12 +58,15 @@ fun ItemsScreen(
     args: ItemsRoute,
     itemsViewModel: ItemsViewModel = hiltViewModel()
 ) {
-    val items by itemsViewModel.getFlowItemsByListId(args.listId).collectAsStateWithLifecycle(initialValue = listOf())
+    val listId = args.listId
+    val listTitle = args.listTitle
+    val items by itemsViewModel.getFlowItemsByListId(listId).collectAsStateWithLifecycle(initialValue = listOf())
     val (readyItems, notReadyItems) = items.partition { it.isReady }
 
-    ItemsScreenContent(notReadyItems,readyItems,
+
+    ItemsScreenContent(listTitle, notReadyItems,readyItems,
         onDeleteItem = { id -> itemsViewModel.deleteItem(id) },
-        addItem = { title -> itemsViewModel.addItem(Item(content = title, listId = args.listId))},
+        addItem = { title -> itemsViewModel.addItem(Item(content = title, listId = listId))},
         onMarkReady = { id, state -> itemsViewModel.changeReadyInItem(id, state) }
     )
 }
@@ -55,6 +74,7 @@ fun ItemsScreen(
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ItemsScreenContent(
+    title: String,
     items: List<Item>,
     readyItems: List<Item>,
     onMarkReady: (Int, Boolean) -> Unit,
@@ -65,54 +85,57 @@ fun ItemsScreenContent(
     val sheetState = rememberModalBottomSheetState()
     val scope = rememberCoroutineScope()
 
-    Box {
-        LazyColumn(
-            Modifier
-                .fillMaxSize()
-                .background(MaterialTheme.colorScheme.background)
-        ) {
-            items(items, key = { it.itemId }) { item ->
-                ItemItem(
-                    text = item.content,
-                    modifier = Modifier.animateItem(),
-                    onReady = { onMarkReady.invoke(item.itemId, true) },
-                    onDeleteList = { onDeleteItem.invoke(item.itemId) })
-            }
-            if (readyItems.isNotEmpty()){
-                item {
-                    HorizontalDivider(
-                        color = Color.LightGray,
-                        thickness = 1.dp,
-                        modifier = Modifier.padding(dimensionResource(id = R.dimen.root_padding))
-                    )
-                }
-
-                items(readyItems, key = { it.itemId }) { item ->
-                    ReadyItem(
+    Column(modifier = Modifier.fillMaxSize()) {
+        TopBar(title = title, onBackClick = { })
+        Box {
+            LazyColumn(
+                Modifier
+                    .fillMaxSize()
+                    .background(MaterialTheme.colorScheme.background)
+            ) {
+                items(items, key = { it.itemId }) { item ->
+                    ItemItem(
                         text = item.content,
                         modifier = Modifier.animateItem(),
-                        onNotReady = {onMarkReady.invoke(item.itemId, false)},
-                        onDeleteItem = { onDeleteItem.invoke(item.itemId) })
+                        onReady = { onMarkReady.invoke(item.itemId, true) },
+                        onDeleteList = { onDeleteItem.invoke(item.itemId) })
+                }
+                if (readyItems.isNotEmpty()) {
+                    item {
+                        HorizontalDivider(
+                            color = Color.LightGray,
+                            thickness = 1.dp,
+                            modifier = Modifier.padding(dimensionResource(id = R.dimen.root_padding))
+                        )
+                    }
+
+                    items(readyItems, key = { it.itemId }) { item ->
+                        ReadyItem(
+                            text = item.content,
+                            modifier = Modifier.animateItem(),
+                            onNotReady = { onMarkReady.invoke(item.itemId, false) },
+                            onDeleteItem = { onDeleteItem.invoke(item.itemId) })
+                    }
                 }
             }
-        }
 
-        FloatingActionButton(
-            onClick = {
-                showBottomSheet = true
-                scope.launch { sheetState.show() }
-            },
-            modifier = Modifier
-                .align(Alignment.BottomEnd)
-                .padding(dimensionResource(id = R.dimen.root_padding))
-        ) {
-            Icon(Icons.Default.Add, contentDescription = "Add Item")
-        }
+            FloatingActionButton(
+                onClick = {
+                    showBottomSheet = true
+                    scope.launch { sheetState.show() }
+                },
+                modifier = Modifier
+                    .align(Alignment.BottomEnd)
+                    .padding(dimensionResource(id = R.dimen.root_padding))
+            ) {
+                Icon(Icons.Default.Add, contentDescription = "Add Item")
+            }
 
-        if (showBottomSheet){
-            AddItemBottomSheet(sheetState, 
-                onDismiss = { showBottomSheet = false }, 
-                addItem = { text -> addItem.invoke(text)})
+            if (showBottomSheet){
+                AddItemBottomSheet(sheetState,
+                    onDismiss = { showBottomSheet = false },
+                    addItem = { text -> addItem.invoke(text)})
+            }
         }
     }
 }
@@ -121,12 +144,12 @@ fun ItemsScreenContent(
 @Composable
 fun ItemsScreenContentPreview() {
     ShoppingListTheme {
-        ItemsScreenContent(listOf(
+        ItemsScreenContent("Title", listOf(
             Item(333,"Item 1", 0,0, false),
             Item(444,"Item 2", 0,1, false)
         ), listOf(
-            Item(333,"Item 1", 0,0, false),
-            Item(444,"Item 2", 0,1, false)
+            Item(55,"Item 1", 0,0, false),
+            Item(44774,"Item 2", 0,1, false)
         ),
             {_,_ -> }, {}, {})
     }
