5af4141c7675495ecc79aab58aa1099cfd8144ed
Dmytro Chkan <chkan.dv@gmail.com>
2024-12-08 17:50:43 +0200

    fix

--- app/src/main/java/chkan/ua/shoppinglist/components/history_list/HistoryUiComponent.kt
+++ app/src/main/java/chkan/ua/shoppinglist/components/history_list/HistoryUiComponent.kt
@@ -1,17 +1,13 @@
 package chkan.ua.shoppinglist.components.history_list
 
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
