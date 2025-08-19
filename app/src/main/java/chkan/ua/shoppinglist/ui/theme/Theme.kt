31267bd1797729ccf508268aad84e6a8818c4fa2
Dmytro Chkan <chkan.dv@gmail.com>
2025-08-19 20:53:04 +0300

    Added new theme

--- app/src/main/java/chkan/ua/shoppinglist/ui/theme/Theme.kt
+++ app/src/main/java/chkan/ua/shoppinglist/ui/theme/Theme.kt
@@ -8,37 +8,61 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80,
+    primary = PrimaryDark,
+    secondary = SecondaryDark,
+    surface = SurfaceDark,
+    surfaceContainer = SurfaceContainerDark,
+    onSurfaceVariant = OnSurfaceVariantDark,
+    onSurface = OnSurfaceDark,
+    onPrimary = OnPrimaryDark,
+    primaryContainer = PrimaryContainerDark,
+    onPrimaryContainer = OnPrimaryContainerDark,
+    secondaryContainer = SecondaryContainerDark,
+    onSecondaryContainer = OnSecondaryContainerDark,
+    outline = OutlineDark,
+    outlineVariant = OutlineVariantDark,
+    surfaceContainerHigh = SurfaceContainerHighDark,
+    surfaceContainerHighest = SurfaceContainerHighestDark,
+    surfaceContainerLow = SurfaceContainerLowDark,
+    surfaceContainerLowest = SurfaceContainerLowestDark,
+    surfaceBright = SurfaceBrightDark,
+    error = Error,
+    onError = OnErrorDark,
+    errorContainer = ErrorContainerDark
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40,
-    secondaryContainer = Color.White
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    primary = Primary,
+    secondary = Secondary,
+    surface = Surface,
+    surfaceContainer = SurfaceContainer,
+    onSurfaceVariant = OnSurfaceVariant,
+    onSurface = OnSurface,
+    onPrimary = OnPrimary,
+    primaryContainer = PrimaryContainer,
+    onPrimaryContainer = OnPrimaryContainer,
+    secondaryContainer = SecondaryContainer,
+    onSecondaryContainer = OnSecondaryContainer,
+    outline = Outline,
+    outlineVariant = OutlineVariant,
+    surfaceContainerHigh = SurfaceContainerHigh,
+    surfaceContainerHighest = SurfaceContainerHighest,
+    surfaceContainerLow = SurfaceContainerLow,
+    surfaceContainerLowest = SurfaceContainerLowest,
+    surfaceBright = SurfaceBright,
+    error = Error,
+    onError = OnError,
+    errorContainer = ErrorContainer
 )
 
 @Composable
 fun ShoppingListTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
@@ -53,7 +77,7 @@ fun ShoppingListTheme(
 
     MaterialTheme(
         colorScheme = colorScheme,
-        typography = AppTypography,
+        typography = Typography,
         content = content
     )
 }
